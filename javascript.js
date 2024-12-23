9092e4dc933e979dd58de4c22b3c4e7eda2f598d
James Hatfield <james.hatfield17@icloud.com>
2024-12-23 20:26:42 +0000

    Add playRound function and logic

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function getComputerChoice() {
     computerChoice = '';
     computerNumber = Math.floor(Math.random() * 3 );
     
-    // Applies logic to select the computer choice
+    // Applies logic to select the computer choice, based on random number generated
     if (computerNumber == 0) {
         computerChoice += 'rock';
     } else if (computerNumber == 1) {
@@ -16,7 +16,7 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// Create getHumanChoice function 
+// Create getHumanChoice function, allows the user to input a choice and is then returned
 function getHumanChoice() {
     userChoice = prompt('Rock, Paper, or Scissors? ').toLowerCase();
 
@@ -28,10 +28,37 @@ function getHumanChoice() {
     } else if (userChoice === 'scissors') {
         return userChoice;
     } else {
-        return 'You entered an invalid choice...';
+        return 'You entered an invalid choice...'; // Done to handle the event of the user inputting unexpected value
     }
 }
 
 // Creating and initializing human and computer score variables
 let humanScore = 0;
 let computerScore = 0;
+
+// Creation of play round function
+function playRound(humanChoice, computerChoice) {
+    // Criteria of human winning
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You win! Rock beats Scissors!');
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You win! Paper beats Rock!');
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win! Scissors beats Paper!');
+    // DRAW criteria    
+    } else if (humanChoice === computerChoice) {
+        console.log('This was a draw!');
+    // Criteria of computer winning
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('Sorry! You lose this round! Paper beats Rock.');
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('Sorry! You lose this round! Scissors beats Paper.');
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('Sorry! You lose this round! Rock beats Paper.');
+    // Criteria of unsupported entry being made
+    } else {
+        console.log('Sorry! Please try again.');
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
