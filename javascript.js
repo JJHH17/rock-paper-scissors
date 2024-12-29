1b9265cc0524a1347834d54c3a2ab8f0750be702
James Hatfield <james.hatfield17@icloud.com>
2024-12-29 17:57:48 +0000

    Add DOM manipulation, result now displays via DIV rather than console.log

--- javascript.js
+++ javascript.js
@@ -32,6 +32,10 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// Create a new DIV which we will store the results of the game in once completed
+const newDiv = document.createElement('div');
+newDiv.id = 'resultDiv';
+
 // Creating and initializing human and computer score variables
 let humanScore = 0;
 let computerScore = 0;
@@ -40,34 +44,48 @@ let computerScore = 0;
 function playGame(humanChoice, computerChoice) {
 
     if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        console.log('You win! Rock beats Scissors!');
+        newDiv.textContent = 'You win! Rock beats Scissors!';
+        document.body.appendChild(newDiv);
         humanScore ++;
+
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        console.log('You win! Paper beats Rock!');
+        newDiv.textContent = 'You win! Paper beats Rock!';
+        document.body.appendChild(newDiv);
         humanScore ++;
+
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log('You win! Scissors beats Paper!');
+        newDiv.textContent = 'You win! Scissors beats Paper!';
+        document.body.appendChild(newDiv);
         humanScore ++;
+
     // DRAW criteria
     // Does not change score
     } else if (humanChoice === computerChoice) {
-        console.log('This was a draw!');
+        newDiv.textContent = 'This was a draw!';
+        document.body.appendChild(newDiv);
+        //console.log('This was a draw!');
+
     // Criteria of computer winning
     // Increments computerScore variable
     } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-        console.log('Sorry! You lose this round! Paper beats Rock.');
+        newDiv.textContent = 'Sorry! You lose this round! Paper beats Rock.';
+        document.body.appendChild(newDiv);
+        //console.log('Sorry! You lose this round! Paper beats Rock.');
         computerScore ++;
+
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        console.log('Sorry! You lose this round! Scissors beats Paper.');
+        newDiv.textContent = 'Sorry! You lose this round! Scissors beats Paper.';
+        document.body.appendChild(newDiv);
+        //console.log('Sorry! You lose this round! Scissors beats Paper.');
         computerScore ++;
+
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log('Sorry! You lose this round! Rock beats Paper.');
+        newDiv.textContent = 'Sorry! You lose this round! Rock beats Paper.';
+        document.body.appendChild(newDiv);
+        //console.log('Sorry! You lose this round! Rock beats Paper.');
         computerScore ++;
-    // Criteria of unsupported entry being made
-    } else {
-        console.log('Sorry! Please try again.');
+        
     }
-    // TODO - Above can be refactored further, making use of OR comparison, reducing lines of code
 
     // print the current scores
     console.log(`The current score is: Human ${humanScore}, Computer ${computerScore}.`);
